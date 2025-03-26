06bc33a1fea3a857c999e274072e47b6703fe641
Alex Bernal <abernal20@unm.edu>
2025-03-26 17:05:30 -0600

    Compute 1/2 and 3/4 of the actual tree limit

--- test_regression_tree.py
+++ test_regression_tree.py
@@ -18,8 +18,18 @@ data = np.column_stack((x, y))
 train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
 
 # Test different limits
+
+# First train an unlimited tree to determine its actual height
+unlimited_tree = RegressionTree(train_data)
+actual_height = unlimited_tree.treeHeight()
+
+# Compute 1/2 and 3/4 of the actual tree height
+height_half = actual_height // 2
+height_three_fourths = (3 * actual_height) // 4
+
+
 # 1/2 of log(100) about 3, 3/4 is about 5.
-limits = [(None, None), (3, None), (5, None), (None, 2), (None, 4), (None, 8)]
+limits = [(None, None), (height_half, None), (height_three_fourths, None), (None, 2), (None, 4), (None, 8)]
 results = []
 
 for height, leaf_size in limits:
